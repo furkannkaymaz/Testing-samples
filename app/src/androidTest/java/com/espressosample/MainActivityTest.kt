86cb3e3bb0521e260f9c6fcefd54e149ecb32b07
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-11 15:30:32 +0300

    check username valid fun added

--- app/src/androidTest/java/com/espressosample/MainActivityTest.kt
+++ app/src/androidTest/java/com/espressosample/MainActivityTest.kt
@@ -3,11 +3,14 @@ package com.espressosample
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -22,10 +25,16 @@ class MainActivityTest {
 
     @Test
     fun checkLogin() {
+
+        Thread.sleep(500)
         onView(withId(R.id.username)).perform(typeText("Furkan"))
+        Thread.sleep(500)
         onView(withId(R.id.password)).perform(typeText("123456"))
+        Thread.sleep(500)
         onView(withId(R.id.login)).perform(click())
 
+        onView(withId(R.id.username)).check(matches(withText("Furkan")))
+        Thread.sleep(500)
     }
 
 }
