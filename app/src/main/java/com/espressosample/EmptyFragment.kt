055370c36cbf00dc055711226c0d4e416d2df3bb
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-13 23:43:47 +0300

    build gradle added

---
+++ app/src/main/java/com/espressosample/EmptyFragment.kt
@@ -0,0 +1,13 @@
+package com.espressosample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EmptyFragment : Fragment(R.layout.fragment_empty) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
