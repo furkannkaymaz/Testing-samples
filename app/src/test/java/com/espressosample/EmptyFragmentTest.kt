b0db29946e74a85a182e408bfcd47750a8bd96ef
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-14 00:04:10 +0300

    first fragment test created

--- app/src/test/java/com/espressosample/EmptyFragmentTest.kt
+++ app/src/test/java/com/espressosample/EmptyFragmentTest.kt
@@ -1,9 +1,13 @@
 package com.espressosample
 
 import android.os.Build
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,9 +24,15 @@ import org.robolectric.annotation.Config
 class EmptyFragmentTest {
     @get:Rule
     val hiltAndroidRule = HiltAndroidRule(this)
+
+    @Before
+    fun setup(){
+        hiltAndroidRule.inject()
+    }
     @Test
     fun testMainActivity() {
         launchFragmentInHiltContainer<EmptyFragment> {
+            onView(withId(R.id.tvEmpty)).check(matches(withText("Hello")))
         }
     }
 }
